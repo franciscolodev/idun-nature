0bbe60ea49ef9793ad84dba0a6d7c188643779a8
franciscolodev <franciscolodev@gmail.com>
2025-07-18 02:45:49 -0300

    actualización idioma

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pl">
+    <html lang="es">
       <body className={inter.className}>
         <header>
           <Header />
